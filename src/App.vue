6d46ec7e6fc1056825923634c770514a60821fda
YuugenP <taikoyuup@hotmail.com>
2023-02-01 20:33:43 +0700

    played with vue a bit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <mainPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mainPage from './components/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mainPage
   }
 }
 </script>
---
+++ src/components/main.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ test }}</h1>
+</template>
+<script>
+
+export default {
+    name: 'mainPage',
+    data(){
+        return {
+            test: 'amogus'
+        }
+    }
+}
+</script>
